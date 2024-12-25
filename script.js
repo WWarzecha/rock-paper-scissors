1659f851427417b3e6e04c74ca4d43445438735b
Woodenspoon <wmwarzecha@gmail.com>
2024-12-25 12:25:09 +0100

    feat: add game loop

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice(){
     // get random number
@@ -28,53 +27,86 @@ function getHumanChoice(){
     return option;
 }
 
-function humanWon(humanChoice, computerChoice){
-    console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
-    humanScore++;
-}
 
-function humanLost(humanChoice, computerChoice){
-    console.log(`You lost! ${computerChoice} beats ${humanChoice}.`);
-    computerScore++;
-}
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
 
-function humanTied(humanChoice, computerChoice){
-    console.log(`It's a tie! ${humanChoice} ties ${computerChoice}.`);
-}
-
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLocaleLowerCase();
-    if (humanChoice === 'rock'){
-        if (computerChoice ==='paper'){
-            humanLost(humanChoice, computerChoice);
+    function humanWon(humanChoice, computerChoice){
+        alert(`You won! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    
+    function humanLost(humanChoice, computerChoice){
+        alert(`You lost! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+    
+    function humanTied(humanChoice, computerChoice){
+        alert(`It's a tie! ${humanChoice} ties ${computerChoice}.`);
+    }
+    
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLocaleLowerCase();
+        if (humanChoice === 'rock'){
+            if (computerChoice ==='paper'){
+                humanLost(humanChoice, computerChoice);
+            }
+            else if (computerChoice === 'scissors'){
+                humanWon(humanChoice, computerChoice);
+            }
+            else{
+                humanTied(humanChoice, computerChoice);
+            }
         }
-        else if (computerChoice === 'scissors'){
-            humanWon(humanChoice, computerChoice);
+        else if(humanChoice === 'paper'){
+            if (computerChoice ==='scissors'){
+                humanLost(humanChoice, computerChoice);
+            }
+            else if (computerChoice === 'rock'){
+                humanWon(humanChoice, computerChoice);
+            }
+            else{
+                humanTied(humanChoice, computerChoice);
+            }
         }
         else{
-            humanTied(humanChoice, computerChoice);
+            if (computerChoice ==='rock'){
+                humanLost(humanChoice, computerChoice);
+            }
+            else if (computerChoice === 'paper'){
+                humanWon(humanChoice, computerChoice);
+            }
+            else{
+                humanTied(humanChoice, computerChoice);
+            }
         }
     }
-    else if(humanChoice === 'paper'){
-        if (computerChoice ==='scissors'){
-            humanLost(humanChoice, computerChoice);
-        }
-        else if (computerChoice === 'rock'){
-            humanWon(humanChoice, computerChoice);
-        }
-        else{
-            humanTied(humanChoice, computerChoice);
-        }
+
+    let humanChoice;
+    let computerChoice;
+
+    alert("Welcome in Rock Paper Scissors game!")
+
+    //5 rounds loop
+        //get human choice
+        //get computer choice
+        //play a round
+
+    for(i = 0; i < 5; i++){
+        humanChoice = getHumanChoice();
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    
+    //decide who is the winner
+    if (humanScore>computerScore){
+        alert("Congratulations, you WON!");
+    }
+    else if (humanScore == computerScore){
+        alert("It's a tie!")
     }
     else{
-        if (computerChoice ==='rock'){
-            humanLost(humanChoice, computerChoice);
-        }
-        else if (computerChoice === 'paper'){
-            humanWon(humanChoice, computerChoice);
-        }
-        else{
-            humanTied(humanChoice, computerChoice);
-        }
+        alert("You lost :(");
     }
 }
