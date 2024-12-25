4ab50198b32b516360a3172b6a713c9b0e5ebb7a
Woodenspoon <wmwarzecha@gmail.com>
2024-12-25 11:38:09 +0100

    feat: add human interaction logic

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function getComputerChoice(){
     // return chosen option
     return option;
 }
+
+// get users choice
+function getHumanChoice(){
+    // ask for input, give 3 options
+    let option = prompt("Give me your choice: \"rock\", \"paper\" or \"scissors\"?");
+    while (!(option === "rock" || option == "paper" || option == "scissors")){
+        if(option === null){
+            option = prompt("You can't cancel my messages. Type in \"rock\", \"paper\" or \"scissors\"?");
+        }
+        else{
+            option = prompt("You spelled it bad. Give me CORRECT choice: \"rock\", \"paper\" or \"scissors\"?");
+        }
+    }
+    // return given input
+    return option;
+}
