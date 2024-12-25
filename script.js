4357504f9ba02686827882db879ca7af476f86e8
Woodenspoon <wmwarzecha@gmail.com>
2024-12-25 11:27:59 +0100

    feat: add computer decision logic

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function getComputerChoice(){
+    // get random number
+    let optionValue = Math.random();
+
+    // decide which option will it be
+    let option = (optionValue > 2/3) ? "rock" : (optionValue > 1/3) ? "paper" : "scissors";
+
+    // return chosen option
+    return option;
+}
